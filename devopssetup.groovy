4d65458772d7fe64537caa5a910c631ba59d4d3a
f2guser <prasanna.upperi@func2grow.com>
2022-04-21 11:05:57 -0400

    initial commit

--- devopssetup.groovy
+++ devopssetup.groovy
@@ -1,21 +1,10 @@
-pipeline {
-    agent any
-    stages {
-        stage('Trigger') {
-            steps {
-                script {
-                	jobDsl targets: 'devops.groovy'
+pipelineJob('test') {
+    definition {
+        cps {
+                script{
+                    jobDsl targets: 'devops.groovy'
                     jobDsl targets: 'devopssetup.groovy', removedJobAction: 'DELETE', removedViewAction: 'DELETE'
                 }
-
-            }
         }
-        //stage('Cleanup') {
-        //    steps {
-        //        script {
-        //        	jobDsl targets: 'jenkins/casc/jobs/devopscleanup.groovy'
-        //        }
-        //    }
-        //}
     }
 }
